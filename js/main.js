e23a281115a426819a01099cb3c0768cde329be0
Yurii Black <95044145+yuriiweblack@users.noreply.github.com>
2024-06-23 12:05:50 +0300

    js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+$('.slider-block__inner').slick({
+    arrows: false,
+    dots:true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
+
+$('.menu__btn').on('click', function() {
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
